18c78b6bcfad9f166a03a64e86d69ae9f09eda1e
Md-Shajib <shajib0607@gmail.com>
2023-02-03 20:42:48 +0600

    truthy vs falsy

---
+++ falsy.js
@@ -0,0 +1,18 @@
+// Falsy Value
+// 0
+// ""
+//undefined
+// null
+// NaN
+
+// Truthy
+// "0"
+// " "
+
+
+let name = " ";
+if(name){
+    console.log("The value is truthy");
+}else{
+    console.log("The value is falsy");
+}
