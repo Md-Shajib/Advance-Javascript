40d7db31d32f9875068c22268edb9c0b52a431f3
Md-Shajib <shajib0607@gmail.com>
2023-02-03 21:01:01 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+// let name;
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    return ;
+}
+
+// const person = {name: "Md Asad", dept: "CSE", phn: 21546312}
+// console.log(person.gf);
+
+// const number = [1,2,3,4,5];
+// console.log(number[15]);
+
+// const addNumber = add(13, 12);
+
+// null
+// const number = null;
+
+const addNumber = add(13, 15);
+console.log(addNumber);
