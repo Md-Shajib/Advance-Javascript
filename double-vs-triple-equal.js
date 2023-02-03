93d6623b051106ced1801893ccd4f30a35a02279
Md-Shajib <shajib0607@gmail.com>
2023-02-03 21:12:12 +0600

    double-vs-triple-equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,21 @@
+// const first = 2;
+// const second = "2";
+// True (==)
+
+// const first = 1;
+// const second = true;
+// True (==)
+
+// const first = 0;
+// const second = false;
+//True (==)
+
+const first = 1;
+const second = true;
+
+if(first === second){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
+// === for checking value with type
